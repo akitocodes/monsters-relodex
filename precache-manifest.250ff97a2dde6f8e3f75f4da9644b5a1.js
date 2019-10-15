052a207da647943fb8c9e4156cf5bbdc27ef14a1
akitocodes <tinynexworks@gmail.com>
2019-10-15 12:59:08 -0700

    Updates

---
+++ precache-manifest.250ff97a2dde6f8e3f75f4da9644b5a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6468326ff40e4e46529a6545827d321e",
+    "url": "/monsters-relodex/index.html"
+  },
+  {
+    "revision": "bc8f17ce8dc1ac82ec24",
+    "url": "/monsters-relodex/static/css/main.edc2ab75.chunk.css"
+  },
+  {
+    "revision": "58e81621b944b883b006",
+    "url": "/monsters-relodex/static/js/2.cb468d4e.chunk.js"
+  },
+  {
+    "revision": "bc8f17ce8dc1ac82ec24",
+    "url": "/monsters-relodex/static/js/main.27efd8b1.chunk.js"
+  },
+  {
+    "revision": "6b3815fb3dbc6049a770",
+    "url": "/monsters-relodex/static/js/runtime-main.f192c6aa.js"
+  }
+]);
